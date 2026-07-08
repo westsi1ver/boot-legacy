17f0ba7a0697ade0cc3b911dd58ebdcf643b027e
westsilver <real.westsilver@gmail.com>
2026-07-08 12:42:26 +0900

    main 2개라 하나 제거

--- src/main/java/org/example/bootlegacy/step2/app/EntryApplication.java
+++ src/main/java/org/example/bootlegacy/step2/app/EntryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // 경로상 ScanController 스캔이 안되서 ㅇㅇ 안되는 것
-@SpringBootApplication
-public class EntryApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EntryApplication.class, args);
-    }
-}
+//@SpringBootApplication
+//public class EntryApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(EntryApplication.class, args);
+//    }
+//}
 
